923a6ca621da6bf63438336aaaedb0948367ce5c
MDavidBenoit <105745980+MDavidBenoit@users.noreply.github.com>
2022-07-27 01:07:05 -0500

    Reconsumo de BD con cambios solicitados

---
+++ Entidades/Incapacidades.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public partial class Incapacidades
+    {
+        public int IdIncapacidad { get; set; }
+        public string Formato { get; set; }
+        public DateTime? FechaInicio { get; set; }
+        public DateTime? FechaFinal { get; set; }
+        public int? IdPosProfe { get; set; }
+
+        public virtual PositivoProfe IdPosProfeNavigation { get; set; }
+    }
+}
