6ade85574bccffe1ad3a20305aefd4476c87db1e
MDavidBenoit <105745980+MDavidBenoit@users.noreply.github.com>
2022-07-31 21:54:29 -0500

    Creación de Font - Incapacidad Profesor con carga de Archivo en Server y Selector de Calendarios

---
+++ Pages/A_Medicos/Agregar_Incapacidad.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Entidades;
+using Logica_de_Negocios;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Seguimineto_COVID.Pages.A_Medicos
+{
+    public partial class Agregar_Incapacidad : System.Web.UI.Page
+    {
+        DLL Interfaz = null;
+        List<Profesor> profesorlist = new List<Profesor>();
+        List<PositivoProfe> positivoPRO = new List<PositivoProfe>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Interfaz = new DLL(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                Session["DLL"] = Interfaz;
+
+                profesorlist = Interfaz.ListaProfesor();
+                DropDownList_selected_Profe.Items.Add("");
+                for (int i = 0; i < profesorlist.Count; i++)
+                {
+                    DropDownList_selected_Profe.Items.Add(profesorlist[i].RegistroEmpleado.ToString());
+                }
+
+            }
+            else
+            {
+                Interfaz = (DLL)Session["DLL"];
+            }
+        }
+
+        protected void Button_guardar_incapacidad_Click(object sender, EventArgs e)
+        {
+            profesorlist = Interfaz.ListaProfesor();
+            positivoPRO = Interfaz.ListaPositivoProfe();
+            byte ultimo = (byte)(positivoPRO.Last().NumContaio + 1);
+            string nombre = "", ruta = "~/Pages/A_Medicos/Incapacidades/", resp = "";
+
+            if (FileUpload_formato.HasFile)
+            {
+                nombre = FileUpload_formato.FileName;
+                ruta = ruta + nombre;
+                FileUpload_formato.SaveAs(Server.MapPath(ruta));
+                resp = "Seguardó el archivo en: " + ruta;
+            }
+
+
+            Incapacidades incapacidad = new Incapacidades()
+            {
+                Formato = ruta,
+                FechaInicio = Calendar_ini.SelectedDate,
+                FechaFinal = Calendar_final.SelectedDate,
+                IdPosProfe = positivoPRO.Where(x => x.IdPosProfe == profesorlist.Where(y => y.RegistroEmpleado == Convert.ToInt32(DropDownList_selected_Profe.SelectedItem.Text)).FirstOrDefault().IdProfe).FirstOrDefault().IdPosProfe
+            };
+
+            Label1.Text = Interfaz.Agregar_Incapacidades(incapacidad) + "-" + resp;
+
+        }
+    }
+}
---
+++ Pages/A_Medicos/Agregar_Incapacidad.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Seguimineto_COVID.Pages.A_Medicos
+{
+
+
+    public partial class Agregar_Incapacidad
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control DropDownList_selected_Profe.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList_selected_Profe;
+
+        /// <summary>
+        /// Control Calendar_ini.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar_ini;
+
+        /// <summary>
+        /// Control Calendar_final.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar_final;
+
+        /// <summary>
+        /// Control FileUpload_formato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload_formato;
+
+        /// <summary>
+        /// Control Button_guardar_incapacidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button_guardar_incapacidad;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+    }
+}
