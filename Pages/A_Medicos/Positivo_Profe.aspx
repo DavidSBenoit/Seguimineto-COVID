b2b022fb7dbd1a0c893aba26d2df708ca6c8c21d
MDavidBenoit <105745980+MDavidBenoit@users.noreply.github.com>
2022-07-31 21:35:32 -0500

    Creación de Font - Positivo Profesor con carga de Archivo en Server y Selector de Calendario

--- Pages/A_Medicos/Positivo_Profe.aspx
+++ Pages/A_Medicos/Positivo_Profe.aspx
@@ -1,18 +1,17 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Positivo_Alumno.aspx.cs" Inherits="Seguimineto_COVID.Pages.A_Medicos.Positivo_Alumno" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Positivo_Profe.aspx.cs" Inherits="Seguimineto_COVID.Pages.A_Medicos.Positivo_Profe" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Positivo Alumno</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Positivo Profesor</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-
 </head>
 <body>
     <form id="form1" runat="server">
-        <%--Nav Bar--%>
+<%--Nav Bar--%>
         <div>
             <nav class="navbar navbar-dark bg-dark fixed-top">
                 <div class="container-fluid">
@@ -65,10 +64,10 @@
         <br />
         <br />
         <%--Pagina--%>
-        <h2>Registra nuevo Caso Positivo de Alumno</h2>
+        <h2>Registra nuevo Caso Positivo de Profesor</h2>
         <p>Ingrese los datos</p>
-        <p>Matrícula:</p>
-        <asp:DropDownList ID="DropDownList_select_alumn" runat="server" OnSelectedIndexChanged="DropDownList_select_alumn_SelectedIndexChanged"></asp:DropDownList>
+        <p>Número de Registro:</p>
+        <asp:DropDownList ID="DropDownList_select_Profe" runat="server"></asp:DropDownList>
 
         <p>Fecha de Confirmado:</p>
         <asp:Calendar ID="Calendar_confirmado" runat="server"></asp:Calendar>
@@ -85,7 +84,7 @@
         <asp:DropDownList ID="DropDownList_Riesgo" runat="server"></asp:DropDownList>
 
         <br />
-        <asp:Button class="btn btn-primary" ID="Button_agregar_Positivo_Alumno" runat="server" Text="Button" OnClick="Button_agregar_Positivo_Alumno_Click" />
+        <asp:Button class="btn btn-primary" ID="Button_agregar_Positivo_Profesor" runat="server" Text="Button" OnClick="Button_agregar_Positivo_Profesor_Click" />
         <br />
         <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
 
---
+++ Pages/A_Medicos/Positivo_Profe.aspx.cs
@@ -0,0 +1,75 @@
+﻿using Entidades;
+using Logica_de_Negocios;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Seguimineto_COVID.Pages.A_Medicos
+{
+
+    public partial class Positivo_Profe : System.Web.UI.Page
+    {
+        DLL Interfaz = null;
+        List<Profesor> profesorlist = new List<Profesor>();
+        List<PositivoProfe> positivoPRO = new List<PositivoProfe>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Interfaz = new DLL(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                Session["DLL"] = Interfaz;
+
+                profesorlist = Interfaz.ListaProfesor();
+                DropDownList_select_Profe.Items.Add("");
+                for (int i = 0; i < profesorlist.Count; i++)
+                {
+                    DropDownList_select_Profe.Items.Add(profesorlist[i].RegistroEmpleado.ToString());
+                }
+
+                DropDownList_Riesgo.Items.Add("Bajo");
+                DropDownList_Riesgo.Items.Add("Medio");
+                DropDownList_Riesgo.Items.Add("Alto");
+            }
+            else
+            {
+                Interfaz = (DLL)Session["DLL"];
+            }
+        }
+
+        protected void Button_agregar_Positivo_Profesor_Click(object sender, EventArgs e)
+        {
+            profesorlist = Interfaz.ListaProfesor();
+            positivoPRO = Interfaz.ListaPositivoProfe();
+            byte ultimo = (byte)(positivoPRO.Last().NumContaio + 1);
+            string nombre = "", ruta = "~/Pages/A_Medicos/Comprobantes_POSPRO/", resp = "";
+
+
+            if (FileUpload_Comprobacion.HasFile)
+            {
+                nombre = FileUpload_Comprobacion.FileName;
+                ruta = ruta + nombre;
+                FileUpload_Comprobacion.SaveAs(Server.MapPath(ruta));
+                resp = "Seguardó el archivo en: " + ruta;
+            }
+
+            PositivoProfe pospro = new PositivoProfe()
+            {
+                FechaConfirmado = Calendar_confirmado.SelectedDate,
+                Comprobacion = ruta,
+                Antecedentes = TextBox_Antecedentes.Text,
+                Riesgo = DropDownList_Riesgo.SelectedItem.Text,
+                NumContaio = ultimo,
+                FProfe = profesorlist.Where(x => x.RegistroEmpleado == Convert.ToInt32(DropDownList_select_Profe.SelectedItem.Text)).FirstOrDefault().IdProfe,
+                Extra = ""
+
+            };
+
+            Label1.Text = Interfaz.Agregar_PositivoProfe(pospro) + "-" + resp;
+        }
+    }
+}
