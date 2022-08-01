c819337c45652dd88ad694a146812360c15ced70
MDavidBenoit <105745980+MDavidBenoit@users.noreply.github.com>
2022-07-31 19:34:55 -0500

    Creación de Font - Positivo Alumno con carga de Archivo en Server y Selector de Calendario

---
+++ Pages/A_Medicos/Positivo_Alumno.aspx.cs
@@ -0,0 +1,80 @@
+﻿using Entidades;
+using Logica_de_Negocios;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Seguimineto_COVID.Pages.A_Medicos
+{
+    public partial class Positivo_Alumno : System.Web.UI.Page
+    {
+        DLL Interfaz = null;
+        List<Alumno> alumnoslist = new List<Alumno>();
+        List<PositivoAlumno> positivoAl = new List<PositivoAlumno>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Interfaz = new DLL(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+                Session["DLL"] = Interfaz;
+
+                alumnoslist = Interfaz.ListaAlumno();
+                DropDownList_select_alumn.Items.Add("");
+                for (int i = 0; i < alumnoslist.Count; i++)
+                {
+                    DropDownList_select_alumn.Items.Add(alumnoslist[i].Matricula);
+                }
+
+                DropDownList_Riesgo.Items.Add("Bajo");
+                DropDownList_Riesgo.Items.Add("Medio");
+                DropDownList_Riesgo.Items.Add("Alto");
+            }
+            else
+            {
+                Interfaz = (DLL)Session["DLL"];
+            }
+
+        }
+
+        protected void DropDownList_select_alumn_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_agregar_Positivo_Alumno_Click(object sender, EventArgs e)
+        {
+            alumnoslist = Interfaz.ListaAlumno();
+            positivoAl = Interfaz.ListaPositivoAlumno();
+            byte ultimo = (byte)(positivoAl.Last().NumContagio + 1);
+            string nombre = "", ruta ="~/Pages/A_Medicos/Comprobantes_POSAL/", resp ="";
+
+
+            if (FileUpload_Comprobacion.HasFile)
+            {
+                nombre = FileUpload_Comprobacion.FileName;
+                ruta = ruta + nombre;
+                FileUpload_Comprobacion.SaveAs(Server.MapPath(ruta));
+                resp = "Seguardó el archivo en: " + ruta;
+            }
+
+            PositivoAlumno posal = new PositivoAlumno()
+            {
+                FechaConfirmado = Calendar_confirmado.SelectedDate,
+                Comprobacion = ruta,
+                Antecedentes = TextBox_Antecedentes.Text,
+                Riesgo = DropDownList_Riesgo.SelectedItem.Text,
+                NumContagio = ultimo,
+                FAlumno = alumnoslist.Where(x => x.Matricula == DropDownList_select_alumn.SelectedItem.Text).FirstOrDefault().IdAlumno,
+                Extra = ""
+                
+            };
+
+            Label1.Text = Interfaz.Agregar_PositivoAlumno(posal) + "-" + resp;
+        }
+    }
+}
---
+++ Pages/A_Medicos/Positivo_Alumno.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Seguimineto_COVID.Pages.A_Medicos
+{
+
+
+    public partial class Positivo_Alumno
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control DropDownList_select_alumn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList_select_alumn;
+
+        /// <summary>
+        /// Control Calendar_confirmado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar_confirmado;
+
+        /// <summary>
+        /// Control FileUpload_Comprobacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUpload_Comprobacion;
+
+        /// <summary>
+        /// Control TextBox_Antecedentes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox_Antecedentes;
+
+        /// <summary>
+        /// Control DropDownList_Riesgo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList_Riesgo;
+
+        /// <summary>
+        /// Control Button_agregar_Positivo_Alumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button_agregar_Positivo_Alumno;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+    }
+}
